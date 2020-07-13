ecaac9f8068d8f2f6dee1075827788900cd9e1e6
Nill <58133705+nillworld@users.noreply.github.com>
2020-07-13 23:58:46 +0900

    Create array1.java
    Fist problem of programmers

---
+++ algogaza_nill/programmers/array1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        
+        
+        
+        solving...........................
+        
+        
+        
+        
+        
+        return answer;
+    }
+}
